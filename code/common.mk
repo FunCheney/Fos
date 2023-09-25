fa0c7c88425622536ab7722c7d7dd3da5cfddeec
Fchen <fc_2012@163.com>
2023-09-25 15:40:55 +0000

    feat: 修改 common.mk 支持 zicsr 扩展

--- code/common.mk
+++ code/common.mk
@@ -1,10 +1,11 @@
 CROSS_COMPILE = riscv64-unknown-elf-
-CFLAGS = -nostdlib -fno-builtin -march=rv32ima -mabi=ilp32 -g -Wall
+CFLAGS = -nostdlib -fno-builtin -march=rv32ima_zicsr -mabi=ilp32 -g -Wall
 
 QEMU = qemu-system-riscv32
 QFLAGS = -nographic -smp 1 -machine virt -bios none
 
-GDB = /opt/riscv/gcc/bin/riscv64-unknown-elf-gdb
+# GDB = /opt/riscv/gcc/bin/riscv64-unknown-elf-gdb
+GDB =  gdb-multiarch
 CC = ${CROSS_COMPILE}gcc
 OBJCOPY = ${CROSS_COMPILE}objcopy
 OBJDUMP = ${CROSS_COMPILE}objdump
