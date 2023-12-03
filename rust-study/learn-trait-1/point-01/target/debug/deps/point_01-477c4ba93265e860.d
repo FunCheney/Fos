5a8940112c3fc0ead86003e02625e04dd7f3ec81
Fchen <fc_2012@163.com>
2023-12-03 22:28:27 +0800

    feat: trait 练习

---
+++ rust-study/learn-trait-1/point-01/target/debug/deps/point_01-477c4ba93265e860.d
@@ -0,0 +1,5 @@
+/Users/fanchen/project/MyProjects/Fos/rust-study/learn-trait-1/point-01/target/debug/deps/point_01-477c4ba93265e860.rmeta: src/main.rs
+
+/Users/fanchen/project/MyProjects/Fos/rust-study/learn-trait-1/point-01/target/debug/deps/point_01-477c4ba93265e860.d: src/main.rs
+
+src/main.rs:
---
+++ rust-study/learn-trait-1/point-01/target/debug/deps/point_01-8f88c9a315064637.d
@@ -0,0 +1,5 @@
+/Users/fanchen/project/MyProjects/Fos/rust-study/learn-trait-1/point-01/target/debug/deps/point_01-8f88c9a315064637.rmeta: src/main.rs
+
+/Users/fanchen/project/MyProjects/Fos/rust-study/learn-trait-1/point-01/target/debug/deps/point_01-8f88c9a315064637.d: src/main.rs
+
+src/main.rs:
