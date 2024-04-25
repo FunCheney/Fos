05b0a0f28c007315b2abeb136f533a2775209c72
Fchen <fc_2012@163.com>
2024-04-25 14:37:26 +0000

    feat: 将立即数加载到 RD 中

--- code/asm/lui/test.s
+++ code/asm/lui/test.s
@@ -1,12 +1,19 @@
+# load upper Immediate
+# Foramt:
+#    LUI RD, IMM 
+# Description
+#    The instruction contains a 20-bit immdeiate value. This value is placed 
+#    in the leftmost (i.e., upper, most significant) 20 bits of the register
+#    RD and the rightmost (i.e., lower, least significant) 12-bits are set
 
-	.text
-	.global _start
+	.text            # Define beginning of text selection
+	.global _start   # Define entry _start
 
 _start:
 	lui x5, 0x12345 # int x5 = 0X12345 << 12
 	addi x5, x5, 0x678  # int x5 = 0x12345000 + 0x678
 
 stop:
-	j stop
+	j stop     # Infinite loop to stop execution
 	
-	.end
+	.end       # End of file
