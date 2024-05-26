2fc9d3b1f29a1cc948b5cdff2325844a62f8dfa6
Fchen <fc_2012@163.com>
2024-05-26 07:19:24 +0000

    feat: update Makefile

--- os/src/main.rs
+++ os/src/main.rs
@@ -6,3 +6,18 @@ use core::arch::global_asm;
 
 global_asm!(include_str!("entry.asm"));
 
+#![no_mangle]
+fn rust_main() -> !{
+    clear_bss();
+    loop {}
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
+}
