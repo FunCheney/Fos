50bec7ee77985f2b840513fb3b1b41add1abda17
Fchen <fc_2012@163.com>
2024-05-30 16:07:40 +0000

    feat: 添加彩色日志

---
+++ os/src/logging.rs
@@ -0,0 +1,56 @@
+
+use log::{self, Level, LevelFilter, Metadata, Record};
+
+struct SimpleLogger;
+
+impl log::Log for SimpleLogger {
+
+    fn enabled(&self, _metadata: &Metadata) -> bool {
+
+        //metadata.level() <= Level::Info
+        true
+        
+    }
+
+    fn log(&self, record: &Record){
+        if !self.enabled(record.metadata()){
+            return;
+        }
+
+        let color = match record.level() {
+            Level::Error => 31, // Red 
+            Level::Warn => 94,
+            Level::Info => 34,
+            Level::Debug => 32,
+            Level::Trace => 90,
+        };
+
+        println!(
+            "\u{1B}[{}m[{:>5}] {}\u{1B}[0m",
+            color,
+            record.level(),
+            record.args(),
+            );
+
+    }
+    
+    fn flush(&self){}
+
+} 
+
+pub fn init() {
+        static LOGGER: SimpleLogger = SimpleLogger;
+        log::set_logger(&LOGGER).unwrap();
+        log::set_max_level(match option_env!("LOG"){
+
+            Some("ERROR") => LevelFilter::Error,
+            Some("WARN") => LevelFilter::Warn,
+            Some("INFO") => LevelFilter::Info,
+            Some("DEBUG") => LevelFilter::Debug,
+            Some("TRACE") => LevelFilter::Trace,
+            _=> LevelFilter::Off
+
+        });
+    }
+
+
--- os/src/main.rs
+++ os/src/main.rs
@@ -6,22 +6,71 @@
 #![no_std]
 #![feature(panic_info_message)]
 
+use core::arch::global_asm;
+use log::*;
+
+
 #[macro_use]
 mod console;
 mod lang_items;
 mod sbi;
+mod logging;
 
-use core::{arch::global_asm};
 
 
 global_asm!(include_str!("entry.asm"));
 
 #[no_mangle]
 fn rust_main() -> !{
+
+    extern "C" {
+        fn stext();
+        fn etext();
+        fn srodata();
+        fn erodata();
+        fn sdata();
+        fn edata();
+        fn sbss();
+        fn ebss();
+        fn boot_stack_top();
+        fn boot_stack_lower_bound();
+    }
+
     clear_bss();
 
-    println!("hello, gjh os");
-    println!("您好, os");
+    logging::init();
+
+    println!("[kernel] hello, gjh os!");
+    //println!("您好, os");
+    trace!(
+        "[kernel] .text [{:#x}, {:#x})",
+        stext as usize,
+        etext as usize
+        );
+
+    debug!(
+        "[kernel] .rodata [{:#x}, {:#x})",
+        srodata as usize,
+        erodata as usize
+        );
+
+    info!(
+        "[kernel] .data [{:#x}, {:#x})",
+        sdata as usize, edata as usize
+        );
+
+    warn!(
+        "[kernel] boot_stack top=bottom={:#x}, lower_bound={:#x}",
+        boot_stack_top as usize,
+        boot_stack_lower_bound as usize
+        );
+
+    error!(
+        "[kernel] .bss [{:#x}, {:#x})",
+        sbss as usize,
+        ebss as usize
+        );
+
 
     panic!("Shutdown machine");
 }
