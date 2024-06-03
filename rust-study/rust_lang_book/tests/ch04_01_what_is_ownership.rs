8bbb58bdbcdbd3d868a8e1787be4695dd3e15503
Fchen <fc_2012@163.com>
2024-06-03 21:15:07 +0800

    feat: 所有权

--- rust-study/rust_lang_book/tests/ch04_01_what_is_ownership.rs
+++ rust-study/rust_lang_book/tests/ch04_01_what_is_ownership.rs
@@ -64,3 +64,63 @@ fn test_04_move() {
     //    |     println!("s1: {s1}, s2: {s2}");
     //    |                   ^^^^ value borrowed here after move
 }
+#[test]
+fn test_owner_ship_fun() {
+    let s = String::from("hello");  // s 进入作用域
+
+    take_owner_ship(s);             // s 的值移动到函数里 ...
+    // ... 所以到这里不再有效
+
+    let x = 5;                      // x 进入作用域
+
+    makes_copy(x);                  // x 应该移动函数里，
+    // 但 i32 是 Copy 的，
+    // 所以在后面可继续使用 x
+}
+pub fn take_owner_ship(some_thing :String){ // some_thing 进入作用域
+    println!("some_thing {some_thing}"); // 使用
+} // 这里 some_string 移出作用域并调用 drop 方法，占用的内存被释放
+
+pub fn makes_copy(some_integer : i32){ // some_integer 进入作用域
+    println!("{}", some_integer);
+}// 这里，some_integer 移出作用域。没有特殊之处
+
+#[test]
+fn test_given_owner_ship() {
+
+    let s1 = gives_ownership();         // gives_ownership 将返回值转移给 s1
+
+    let s2 = String::from("hello");  // s2 进入作用域
+
+    let s3 = takes_and_gives_back(s2);  // s2 被移动到 takes_and_gives_back 中，它也将返回值移给 s3
+}// 这里，s3 移出作用域并被丢弃。s2 也移出作用域，但已被移走，所以什么也不会发生。s1 离开作用域并被丢弃
+
+fn gives_ownership() -> String {   // gives_ownership 会将返回值移动给调用它的函数
+
+    let some_string = String::from("yours"); // some_string 进入作用域。
+
+    some_string    // 返回 some_string 并移出给调用的函数
+}
+
+// takes_and_gives_back 将传入字符串并返回该值
+fn takes_and_gives_back(a_string: String) -> String { // a_string 进入作用域
+    a_string  // 返回 a_string 并移出给调用的函数
+}
+
+/// 变量的所有权总是遵循相同的模式：将值赋给另一个变量时移动它。当持有堆中数据的变量离开作用域时，
+/// 其值将通过 drop 被清理掉，除非数据被移动为另一个变量所有。
+
+#[test]
+fn test_06() {
+    let s1 = String::from("hello");
+
+    let (s2, len) = calculate_length(s1);
+    // 这一行会报错，s1 的所有权已经被转移
+    // println!("s1 {s1}");
+    println!("The length of '{}' is {}.", s2, len);
+}
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() 返回字符串的长度
+
+    (s, length)
+}
