648ac806e734b28d0adae4a5e33bcb7da35813da
Fchen <fc_2012@163.com>
2024-06-09 15:27:48 +0800

    feat: trait 实现

--- rust-study/rust_lang_book/tests/ch10_01_syntax.rs
+++ rust-study/rust_lang_book/tests/ch10_01_syntax.rs
@@ -45,3 +45,48 @@ impl <T> Point<T> {
         &self.x
     }
 }
+
+struct Point3<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl <X1, Y1> Point3<X1, Y1> {
+    fn mixup <X2,Y2>(self, other: Point3<X2, Y2>) -> Point3<X1, Y2> {
+        Point3{
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+#[test]
+fn test_03() {
+    let p1 = Point3 { x: 5, y: 10.4 };
+    let p2 = Point3 { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
+/// 泛型代码的性能
+/// 泛型并不会比具体的类型运行慢
+/// Rust 会在编译时对泛型代码进行单态化来保证效率。单态化是一个通过填充编译时使用的具体类型，将通用代码转换为特定代码的过程。
+
+#[test]
+fn test_04() {
+    let integer = Some(5);
+    let float = Some(5.0);
+    // 当 Rust 编译这些代码时会进行单态化，编译器会读取传递给 Option<T> 的值并发现有两种Option<T>：一个对应 i32 另一个对应 f64。
+    // 它会将泛型定义 Option<T> 展开为两个针对 i32 和 f64 的定义，接着将泛型定义替换为这两个具体的定义。
+}
+/// 编译器生成的单态化版本的代码看起来像这样（编译器会使用不同于如下假想的名字）：
+enum Option_i32 {
+    Some(i32),
+    None,
+}
+
+enum Option_f64 {
+    Some(f64),
+    None,
+}
