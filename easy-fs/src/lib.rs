062d2c312df40cb85cc4dfb61eb3c0e0b134c4a7
Fchen <fc_2012@163.com>
2024-09-30 22:27:39 +0800

    feat: 块设备接口层

--- easy-fs/src/lib.rs
+++ easy-fs/src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![no_std]
+#![deny(missing_docs)]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod block_dev;
