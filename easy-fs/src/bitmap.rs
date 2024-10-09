7a0ac109912c987a8ee782107375ed9523129191
Fchen <fc_2012@163.com>
2024-10-09 00:33:44 +0000

    feat: 简易文件系统

--- easy-fs/src/bitmap.rs
+++ easy-fs/src/bitmap.rs
@@ -1,8 +1,11 @@
+use core::u64;
+
+use alloc::sync::Arc;
+
 use crate::{block_cache::get_block_cache, BlockDevice, BLOCK_SIZE};
 
 type BitMapBlock = [u64; 64];
 const BLOCKS_BITS: usize = BLOCK_SIZE * 8;
-
 
 pub struct BitMap {
     start_block_id: usize,
@@ -18,11 +21,46 @@ impl BitMap {
     }
 
     pub fn alloc(&self, block_device: &Arc<dyn BlockDevice>) -> Option<usize> {
-        for block_id in 0..self.blocks  {
+        for block_id in 0..self.blocks {
             let pos = get_block_cache(
                 block_id + self.start_block_id as usize,
                 Arc::clone(block_device),
-                ).lock().modify(offset, f)
+            )
+            .lock()
+            .modify(0, |bitmap_block: &mut BitMapBlock| {
+                if let Some((bits64_pos, inner_pos)) = bitmap_block
+                    .iter()
+                    .enumerate()
+                    .find(|(_, bits64)| **bits64 != u64::MAX)
+                    .map(|(bits64_pos, bits64)| (bits64_pos, bits64.trailing_ones() as usize))
+                {
+                    bitmap_block[bits64_pos] |= 1u64 << inner_pos;
+                    Some(block_id * BLOCKS_BITS + bits64_pos * 64 + inner_pos as usize)
+                } else {
+                    None
+                }
+            });
+
+            if pos.is_some() {
+                return pos;
+            }
         }
+        None
     }
+
+    pub fn delloc(&self, block_device: &Arc<dyn BlockDevice>, bit: usize) {
+        let (block_pos, bits64_pos, inner_pos) = decomposition(bit);
+        get_block_cache(block_pos + self.start_block_id, Arc::clone(block_device))
+            .lock()
+            .modify(0, |bitmap_block: &mut BitMapBlock| {
+                assert!(bitmap_block[bits64_pos] & (1u64 << inner_pos) > 0);
+                bitmap_block[bits64_pos] -= 1u64 << inner_pos;
+            });
+    }
+}
+
+pub fn decomposition(mut bit: usize) -> (usize, usize, usize) {
+    let block_pos = bit / BLOCKS_BITS;
+    bit = bit % BLOCKS_BITS;
+    (block_pos, bit / 64, bit % 64)
 }
--- easy-fs/src/layout.rs
+++ easy-fs/src/layout.rs
@@ -1,5 +1,16 @@
+use crate::BLOCK_SIZE;
+
 /// Magic number for sanity check
 const EFS_MAGIC: u32 = 0x3b800001;
+const INODE_DIRECT_COUNT: usize = 28;
+
+/// The upper bound of direct inode index
+const DIRECT_BOUND: usize = INODE_DIRECT_COUNT;
+
+const INODE_INDIRECT1_COUNT: usize = BLOCK_SIZE / 4;
+const INDIRECT1_BOUND: usize = DIRECT_BOUND + INODE_INDIRECT1_COUNT;
+
+
 
 #[repr(C)]
 pub struct SupperBlock {
@@ -20,7 +31,7 @@ impl SupperBlock {
         data_bit_map_blocks: u32,
         data_area_blocks: u32,
     ) {
-        *self = self {
+        *self = Self {
             magic: EFS_MAGIC,
             total_blocks,
             inode_bitmap_blocks,
@@ -32,5 +43,43 @@ impl SupperBlock {
 
     pub fn is_validate(&self) -> bool {
         self.magic == EFS_MAGIC
+    }
+}
+
+#[derive(PartialEq)]
+pub enum DiskInodeType {
+    File,
+    Directory,
+}
+
+
+type IndirectBlock = [u32; BLOCK_SZ / 4];
+
+#[rep(C)]
+pub struct DiskInode {
+    pub size: u32,
+    pub direct: [u32; INODE_DIRECT_COUNT],
+    pub indirect1: u32,
+    pub indirect2: u32,
+    type_: DiskInodeType,
+}
+
+
+
+impl DiskInode {
+    pub fn initialize(&mut self,type_: DiskInodeType) {
+        self.size = 0;
+        self.direct.iter_mut(),for_each(|v| *v = 0);
+        self.indirect1 = 0;
+        self.indirect2 = 0;
+        self.type_ = type_;
+    }
+
+    pub fn is_dir(&self) -> bool {
+        self.type_ == DiskInodeType::Directory
+    }
+
+    pub fn is_file(&self) -> bool {
+        self.type_ == DiskInodeType::File
     }
 }
