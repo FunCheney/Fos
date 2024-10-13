aba341e40195a08de9ddba7969bd54b80fe78372
Fchen <fc_2012@163.com>
2024-10-13 09:33:08 +0000

    feat: 内核中 fs 实现

---
+++ os/src/fs/inode.rs
@@ -0,0 +1,44 @@
+use alloc::{sync::Arc, vec::Vec};
+use easy_fs::Inode;
+use lazy_static::lazy_static;
+
+use crate::sync::UPSafeCell;
+
+pub struct OSInode {
+    readable: bool,
+    writable: bool,
+    inner: UPSafeCell<OSInodeInner>,
+}
+
+pub struct OSInodeInner {
+    offset: usize,
+    inode: Arc<Inode>,
+}
+
+impl OSInode {
+    pub fn new(readable: bool, writable: bool, inode: Arc<Inode>) -> Self {
+        Self {
+            readable,
+            writable,
+            inner: unsafe { UPSafeCell::new(OSInodeInner { offset: 0, inode }) },
+        }
+    }
+
+    pub fn read_all(&self) -> Vec<u8> {
+        let mut inner = self.inner.exclusive_access();
+        let mut buffer = [0u8; 512];
+        let mut v: Vec<u8> = Vec::new();
+        loop {
+            let len = inner.inode.read_at(inner.offset, &mut buffer);
+            if len == 0 {
+                break;
+            }
+
+            inner.offset += len;
+            v.extend_from_slice(&buffer[..len]);
+        }
+        v
+    }
+}
+
+lazy_static! {}
