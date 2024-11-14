b31f14851821fe527b44d906ffbe5d8c6d29ebc5
Fchen <fc_2012@163.com>
2024-11-14 08:40:22 +0800

    feat: cell & refcell

--- rust-study/rust-atomics-and-locks/examples/ch1-06-cell.rs
+++ rust-study/rust-atomics-and-locks/examples/ch1-06-cell.rs
@@ -1,1 +1,16 @@
-fn main() {}
+use std::cell::Cell;
+
+fn main() {
+    let v = Cell::new(vec![1,2,3]);
+    f(&v);
+    assert_eq!(v.into_inner(), vec![1,2,3,5]);
+}
+
+fn f(v: &Cell<Vec<i32>>){
+    // Replaces the contents of the Cell with an empty Vec
+    let mut v2 = v.take();
+    println!("v2 {:?}", v2);
+    v2.push(5);
+    // // Put the modified Vec back
+    v.set(v2)
+}
