87490bb54597935315fe064d9622cea149b20181
Fchen <fc_2012@163.com>
2024-11-15 19:01:13 +0800

    feat: 修改代码

--- rust-study/rust-atomics-and-locks/examples/ch1-12-condvar.rs
+++ rust-study/rust-atomics-and-locks/examples/ch1-12-condvar.rs
@@ -1,1 +1,33 @@
-fn main() {}
+use std::collections::VecDeque;
+use std::sync::{Condvar, Mutex};
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let queue = Mutex::new(VecDeque::new());
+    let notify_empty = Condvar::new();
+
+    thread::scope(|s| {
+       s.spawn(|| {
+           loop {
+               let mut q = queue.lock().unwrap();
+               let  item = loop {
+                   if let Some(item) = q.pop_front() {
+                       break item;
+                   } else {
+                       q = notify_empty.wait(q).unwrap();
+                   }
+               };
+
+               drop(q);
+               dbg!(item);
+           }
+       });
+
+        for i in 0.. {
+            queue.lock().unwrap().push_back(i);
+            notify_empty.notify_one();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+}
