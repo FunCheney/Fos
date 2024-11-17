ebbe121bf9a0c2f6ddea0e771a5fd5a3dff8e356
Fchen <fc_2012@163.com>
2024-11-17 23:16:13 +0800

    feat: update

---
+++ rust-study/rust-atomics-and-locks/examples/ch2-01-stop-flag.rs
@@ -0,0 +1,30 @@
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering::Relaxed;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    static STOP: AtomicBool = AtomicBool::new(false);
+
+    let background_thread = thread::spawn(|| {
+        while !STOP.load(Relaxed) {
+            some_work();
+        }
+    });
+
+    for line in std::io::stdin().lines() {
+        match line.unwrap().as_str() {
+            "help" => println!("commands: help, stop"),
+            "stop" => break,
+            cmd=> println!("unknown command: {cmd:?}"),
+        }
+    }
+
+    STOP.store(true, Relaxed);
+
+    background_thread.join().unwrap();
+}
+
+fn some_work() {
+    thread::sleep(Duration::from_millis(100));
+}
