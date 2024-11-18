0eea0699297f313ce1103c424b12249a0a49ba61
Fchen <fc_2012@163.com>
2024-11-18 18:34:10 +0800

    feat: atomics

--- rust-study/rust-atomics-and-locks/examples/ch2-07-statistics.rs
+++ rust-study/rust-atomics-and-locks/examples/ch2-07-statistics.rs
@@ -1,1 +1,47 @@
-fn main() {}
+use std::sync::atomic::{AtomicU64, AtomicUsize};
+use std::sync::atomic::Ordering::Relaxed;
+use std::thread;
+use std::time::{Duration, Instant};
+
+fn main() {
+    let num_done = &AtomicUsize::new(0);
+    let total_time = &AtomicU64::new(0);
+    let max_time = &AtomicU64::new(0);
+
+    thread::scope(|s| {
+        for t in 0..4 {
+            s.spawn(move || {
+                for i in 0..25 {
+                    let start = Instant::now();
+                    process_item(t * 25 + i);
+                    let time_taken = start.elapsed().as_micros() as u64;
+                    num_done.fetch_add(1, Relaxed);
+                    max_time.fetch_max(time_taken, Relaxed);
+                }
+            });
+        }
+
+        loop {
+            let total_time = Duration::from_micros(total_time.load(Relaxed));
+            let max_time = Duration::from_micros(max_time.load(Relaxed));
+            let n = num_done.load(Relaxed);
+            if n == 100 { break; }
+            if n == 0 {
+                println!("working.. nothing done yet.");
+            }else {
+                println!(
+                    "working..{n}/100 done, {:?} average, {:?} peak",
+                    total_time / n as u32,
+                    max_time
+                );
+            }
+            thread::sleep(Duration::from_secs(1))
+        }
+
+    });
+    println!("done")
+}
+
+fn process_item(_: usize) {
+    thread::sleep(Duration::from_millis(500));
+}
