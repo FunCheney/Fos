6baf276a334da4b9206a77500119cbc02d1ec605
Fchen <fc_2012@163.com>
2024-12-25 11:04:43 +0800

    feat: 修改代码

--- rust-study/cli/src/main.rs
+++ rust-study/cli/src/main.rs
@@ -1,13 +1,18 @@
+use std::path::Path;
 // cli csv -i input.csv -o output json —header -d ','
-use clap::{command, parser};
-#[derive(Debug, PartialEq)]
-#[command (name= "cli", version, author, about, long_about = None) ]
+use clap::{Parser};
+use csv::{Reader, StringRecord};
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Parser)]
+#[command(name= "cli", version, author, about, long_about = None) ]
 struct Opts {
-    #[commamd(subcommand)]
+    #[command(subcommand)]
     cmd: Subcommand,
 
 }
 
+#[derive(Debug, Parser)]
 enum Subcommand {
     #[command(name="csv", about = "show CSV, or Convert CSV file to other formats")]
     Csv(CsvOpts),
@@ -21,17 +26,37 @@ pub struct CsvOpts {
     #[arg(short, long)] // "output.json".into()
     pub output: Option<String>,
 
-    #[arg(long, value_parser = parse_format, default_value = "json")]
-    pub format: OutputFormat,
-
     #[arg(short, long, default_value_t = ',')]
     pub delimiter: char,
 
     #[arg(long, default_value_t = true)]
     pub header: bool,
 }
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Record {
+    pub name: String,
+    pub age: u8,
+}
 
 fn main() {
-
+    let opts = Opts::parse();
+    println!("{:?}", opts);
+    match opts.cmd {
+        Subcommand::Csv(opts) => {
+            let mut reader = Reader::from_path(opts.input).unwrap();
+            let records = reader.records()
+            .map(|r| r.unwrap())
+                .collect::<Vec<StringRecord>>();
+            println!("{:?}", records);
+        }
+    }
     println!("Hello, world!");
+}
+
+fn verify_file(path: &str) -> Result<String, String> {
+    if Path::new(path).exists() {
+        Ok(path.into())
+    }else {
+        Err(format!("{} does not exist", path))
+    }
 }
